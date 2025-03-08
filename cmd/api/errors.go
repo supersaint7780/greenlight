3cd2c6024abaaa71d2b44495e84f232c633a4fc1
Ayush <hackerboy5556@gmail.com>
2025-03-08 14:17:34 +0530

    refactor error handling

---
+++ cmd/api/errors.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// generic method to log an error message
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+// generic method for sending JSON-formatted error response
+// with a status code to the client.
+// We are using any for the message parameter rather than string
+// becuase it gives us more flexibility regarding values we
+// can include in the reponse
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message any) {
+	env := envelope{"error": message}
+	err := app.writeJSON(w, status, env, nil)
+
+	// is writing error as json response fails
+	// send an empty response with a 500 Internal server error
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+	message := "The server encountered a problem and could not process your request"
+	app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (app *application) notFoundRepsonse(w http.ResponseWriter, r *http.Request) {
+	message := "The requested resource could not be found"
+	app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	message := fmt.Sprintf("The %s method is not allowed for this resource", r.Method)
+	app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
+}
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,7 +15,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundRepsonse(w, r)
 		return
 	}
 
@@ -30,8 +30,6 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "An error occured while processing your request", http.StatusInternalServerError)
+		app.serverErrorResponse(w, r, err)
 	}
-
 }
